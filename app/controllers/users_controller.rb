bd8367094cf94f545f08f13583a2946bb1d785ca
Elle Harding <ehardin0@chicagobooth.edu>
2023-05-04 14:16:25 +0000

    Sign In Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
+ 
+  def authenticate
+    un = params.fetch("input_username")
+    pw = params.fetch("input_password")
+
+    user = User.where({ :username => un }).at(0)
+
+    if user == nil
+      redirect_to("/user_sign_in", { :alert => "No one by that name here "})
+    else
+      if user.authenticate(pw)
+        session.store( :user_id, user.id)
+        redirect_to("/", { :notice => "Welcome back, " + user.username + "!" })
+      else
+        redirect_to("/user_sign_in", { :alert => "Incorrect Password. Try again!"})
+      end
+    end
+  end 
+
+  def new_session
+    render({ :template => "users/sign_in.html.erb" })
+  end   
+  
+  def toast_cookies
+    reset_session
+
+    redirect_to("/", { :notice => "See ya later!"})
+  end
   
   def new_registration_form
   render({ :template => "users/signup_form.html.erb" })
@@ -27,6 +55,7 @@ class UsersController < ApplicationController
     save_status = user.save
 
     if save_status == true
+      session.store(:user_id, user.id)
       redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!" })
     else
       redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence })
